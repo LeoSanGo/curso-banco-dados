1b9e4d33e831db596ebb806d9e70e04598e4cbaf
leosango <souoleo@gmail.com>
2022-01-09 23:58:21 -0300

    file to add data totable planetas

---
+++ primeiros-passos/inserir-planeta.sql
@@ -0,0 +1,29 @@
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (600, 'Abafar', 605857.00, null, 1);
+
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (400, 'Alderaan', 6250, 0, 1);
+
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (300, 'Tatooine', 5247.50, 3, 1);
+
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (400, 'Sullust', null, 3, 1);
+
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (500, 'Saturno', 58232.00, 7, 0);
+
+INSERT INTO
+  planeta (id, nome, raio, qtde_luas, habitado)
+VALUES
+  (600, 'Abafar', 605857.00, null, 1);
