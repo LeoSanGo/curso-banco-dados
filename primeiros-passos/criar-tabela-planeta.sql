1e0a1eb8c04e059b130a016b1904b18a8b168567
leosango <souoleo@gmail.com>
2022-01-10 23:47:52 -0300

    Add codes to update personagem and planeta

--- primeiros-passos/criar-tabela-planeta.sql
+++ primeiros-passos/criar-tabela-planeta.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS planeta (
   id INT,
   nome VARCHAR(80),
   raio DECIMAL(8, 2),
-  qntde_luas INT, 
+  qtde_luas INT, 
   habitado BOOLEAN
 );   
---
+++ primeiros-passos/seleconar-personagem-2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM personagem;
+
+SELECT nome, id FROM personagem;
+
+SELECT nome, id FROM personagem WHERE id = 5;
+
+SELECT nome, id FROM personagem WHERE BINARY nome = 'Chewbacca';
+
+SELECT altura * 2 as dobro FROM personagem;
+
+SELECT * FROM personagem WHERE 0;
+
+SELECT * FROM personagem WHERE 1;
